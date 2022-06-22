6a311ac90091f51c524001dafda077f2e2a68e2f
seanpconroy <seanpconroy3@gmail.com>
2022-06-22 14:26:57 -0400

    message

---
+++ prime.java
@@ -0,0 +1,34 @@
+public class PrimeExample
+{    
+ public static void main(String args[])
+ {    
+  String aws_access_key_id= "AKIAIOSFODNN7EXAMPLE"
+  String aws_secret_access_key= "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+  
+  int i,m=0,flag=0;      
+  int n=3;//it is the number to be checked    
+  m=n/2;      
+  if(n==0||n==1)
+  {  
+   System.out.println(n+" is not prime number");      
+  }
+  String username = "seanconroy"
+  String password = "password_find_me"
+  else
+  {  
+   for(i=2;i<=m;i++)
+   {      
+    if(n%i==0)
+    {      
+     System.out.println(n+" is not prime number");      
+     flag=1;      
+     break;      
+    }      
+   }      
+   if(flag==0)  
+   { 
+    System.out.println(n+" is prime number"); 
+    }  
+  }//end of else  
+}    
+}
